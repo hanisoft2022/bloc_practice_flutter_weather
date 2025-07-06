52527e0235842aa0b0585393835917e08465bac3
hani <hanisoft2022@gmail.com>
2025-07-06 13:51:22 +0900

    open_meteo_api_client.dart config complete

--- packages/open_meteo_api/lib/src/models/location.dart
+++ packages/open_meteo_api/lib/src/models/location.dart
@@ -1,4 +1,14 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'location.g.dart';
+
+@JsonSerializable()
 class Location {
+  final int id;
+  final String name;
+  final double latitude;
+  final double longitude;
+
   const Location({
     required this.id,
     required this.name,
@@ -6,11 +16,5 @@ class Location {
     required this.longitude,
   });
 
-  final int id;
-  final String name;
-  final double latitude;
-  final double longitude;
+  factory Location.fromJson(Map<String, dynamic> json) => _$LocationFromJson(json);
 }
-
-// for git commit
-// for git commit #2
--- packages/open_meteo_api/lib/src/models/weather.dart
+++ packages/open_meteo_api/lib/src/models/weather.dart
@@ -1,6 +1,14 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'weather.g.dart';
+
+@JsonSerializable()
 class Weather {
+  final double temperature;
+  @JsonKey(name: 'weathercode')
+  final double weatherCode;
+
   const Weather({required this.temperature, required this.weatherCode});
 
-  final double temperature;
-  final double weatherCode;
+  factory Weather.fromJson(Map<String, dynamic> json) => _$WeatherFromJson(json);
 }
